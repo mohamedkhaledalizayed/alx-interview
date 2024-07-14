21e7feeaebcbf93eb6cc2f23990d3feb30b48b75
Mohamed Zayed <mohammedkhaled077@gmail.com>
2024-07-14 23:38:49 +0200

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
+
 """
-This module contains a function to calculate the minimum number of operations
-needed to result in exactly n H characters in the file.
+Module that contains a method that calculates
+the fewest number of operations needed
+to result in exactly n H characters in the file.
 """
+
 
 def minOperations(n):
-    """
-    Calculate the fewest number of operations needed to result in exactly n H characters.
-    
-    :param n: Target number of H characters
-    :type n: int
-    :return: Minimum number of operations or 0 if n is impossible to achieve
-    :rtype: int
-    """
-    if n <= 1:
+    operations = 0
+    factor = 2
+
+    if n < 0:
         return 0
 
-    operations = 0
-    divisor = 2
-
     while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+
+        factor += 1
+
+        if factor * factor > n:
+            if n > 1:
+                operations += n
+            break
 
     return operations
