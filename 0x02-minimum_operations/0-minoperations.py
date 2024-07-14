9e9e6f00774ead7cfa9caeb059cae2044de1c749
Mohamed Zayed <mohammedkhaled077@gmail.com>
2024-07-14 23:35:06 +0200

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-'''A module for working with lockboxes.
-'''
+"""
+This module contains a function to calculate the minimum number of operations
+needed to result in exactly n H characters in the file.
+"""
 
+def minOperations(n):
+    """
+    Calculate the fewest number of operations needed to result in exactly n H characters.
+    
+    :param n: Target number of H characters
+    :type n: int
+    :return: Minimum number of operations or 0 if n is impossible to achieve
+    :rtype: int
+    """
+    if n <= 1:
+        return 0
 
-def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
-            continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
